1a858255953b27090bc83b41eeba36e62248922e
Oscar Hdz <oscardavid.hernandez.mx@gmail.com>
2020-02-12 22:45:01 -0600

    Steup (cleanup) master next app & added bootstrap

--- packages/master-app/components/nav.js
+++ packages/master-app/components/nav.js
@@ -2,26 +2,23 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => ({
-  ...link,
-  key: `nav-link-${link.href}-${link.label}`,
-}))
+  { href: '/', label: 'Home' },
+];
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
+  <nav style={{backgroundColor: '#eceff1', borderBottom: '3px solid #545454'}}>
+    <ul style={{padding: '14px 16px', margin: '0'}}>
+      {
+        links.map(link => {
+          return (
+            <li key={link.label.toLowerCase()}>
+              <Link href={link.href}>
+                <a>{link.label}</a>
+              </Link>
+            </li>
+          )
+        })
+      }
     </ul>
 
     <style jsx>{`
