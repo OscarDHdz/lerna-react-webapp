07009368e5ce5640dddb22773d913b366f16450c
Oscar Hdz <oscardavid.hernandez.mx@gmail.com>
2020-02-12 23:45:05 -0600

    Add sub next app (page-about) to master-app

--- packages/master-app/components/nav.js
+++ packages/master-app/components/nav.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 const links = [
   { href: '/', label: 'Home' },
+  { href: '/about', label: 'About'}
 ];
 
 const Nav = () => (
---
+++ packages/master-app/pages/about.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import Nav from '../components/nav';
+import PageAbout from 'page-about';
+
+const Home = () => (
+  <Fragment>
+    <Nav />
+    <PageAbout/>
+  </Fragment>
+)
+
+export default Home
