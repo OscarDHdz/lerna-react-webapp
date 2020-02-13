3a2e7082af457d454a126e02f8b7abc8f5d62ae7
Oscar Hdz <oscardavid.hernandez.mx@gmail.com>
2020-02-12 23:13:43 -0600

    Init sub next-app (page-about)

---
+++ packages/page-about/components/PageAbout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PageAbout = () => {
+  return (
+    <h1>About us...</h1>
+  )
+}
+
+export default PageAbout;
---
+++ packages/page-about/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageAbout from '../components/PageAbout';
+
+const App = () => {
+  return (
+    <PageAbout/>
+  )
+}
+
+export default App;
