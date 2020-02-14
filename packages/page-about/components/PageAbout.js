b8e5d2e5bd1903920121ecaabf4d79a84b77c9e0
Oscar Hdz <oscardavid.hernandez.mx@gmail.com>
2020-02-13 20:11:30 -0600

    Added components-bundle to page-about

--- packages/page-about/components/PageAbout.js
+++ packages/page-about/components/PageAbout.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import Cards from './Cards';
 import Header from './Header';
+import {AwesomeButton, LoadingIcon} from 'components-bundle';
 
 const PageAbout = () => {
   return (
@@ -11,6 +12,15 @@ const PageAbout = () => {
         
         <p>Also... here you can see some bootstrap cards:</p>
         <Cards/>
+
+        <br/>
+
+        <div style={{backgroundColor: 'black', color: 'white', padding: '7px', borderRadius: '8px'}}>
+          <p>
+            And yup, I also have some elements of the <code>components-bundle</code>: <br/>
+            A spinner: <LoadingIcon/> An awesome button: <AwesomeButton/>
+          </p>
+        </div>
 
 
       </div>
